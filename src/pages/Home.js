0dca0bb63fcd154b845760a971aad27bd96175c6
progg1992 <roggpierce@gmail.com>
2021-02-09 11:17:23 -0600

    added jumbotron to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import Hero from '../components/Hero';
+
 function HomePage(props) {
 
     return (
-        <p>Home Works</p>
+        <div>
+            <Hero title={props.title} subtitle={props.subTitle} text={props.text} />
+
+        </div>
     )
 }
 
-export default HomePage
+export default HomePage;
